31de5fcf5c0e71b80945b02532c6b894e25df4d5
Max B. <77401015+maxboro@users.noreply.github.com>
2025-02-18 22:51:47 +0200

    Add simplified example (#3)
    * Create shared ptr to queue
    
    * Add minimal consumer-producer example
    
    * Make queue operation thread-safe
    
    * Add example to README

--- include/main.hpp
+++ include/main.hpp
@@ -3,26 +3,24 @@
 
 #include <iostream>
 #include <queue>
+#include <memory>
+#include <mutex>
 #include "producer.hpp"
 #include "consumer.hpp"
 
 // Simple queue use example
 int main(){
     std::cout << "Running" << std::endl;
-    std::queue <int> queue_inst;
+    auto queue_ptr = std::make_shared<std::queue<int>>();
+    auto mutex_ptr = std::make_shared<std::mutex>();
+    
+    std::thread producer_thread(produce, queue_ptr, mutex_ptr);
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    std::thread consumer_thread(consume, queue_ptr, mutex_ptr);
 
-    // put elements to queue
-    for (int i = 0; i < 10; i++){
-        queue_inst.push(i);
-    }
-
-    // Read from queue
-    while (!queue_inst.empty()) {
-        int element = queue_inst.front();
-        std::cout << "Element from queue: " << element << std::endl;
-        queue_inst.pop();
-    }
-    std::cout << "Finished execution" << std::endl;
+    producer_thread.join();
+    consumer_thread.join();
+    std::cout << "Execution is finished" << std::endl;
     return 0;
 }
 
