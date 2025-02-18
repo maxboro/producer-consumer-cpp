d137194ed36addfabc96f41ef7a25fff56665a29
Max B. <maksym_borovyk@ukr.net>
2025-02-18 22:10:08 +0200

    Create shared ptr to queue

--- include/main.hpp
+++ include/main.hpp
@@ -3,24 +3,25 @@
 
 #include <iostream>
 #include <queue>
+#include <memory>
 #include "producer.hpp"
 #include "consumer.hpp"
 
 // Simple queue use example
 int main(){
     std::cout << "Running" << std::endl;
-    std::queue <int> queue_inst;
+    auto queue_inst = std::make_shared<std::queue<int>>();
 
     // put elements to queue
     for (int i = 0; i < 10; i++){
-        queue_inst.push(i);
+        queue_inst->push(i);
     }
 
     // Read from queue
-    while (!queue_inst.empty()) {
-        int element = queue_inst.front();
+    while (!queue_inst->empty()) {
+        int element = queue_inst->front();
         std::cout << "Element from queue: " << element << std::endl;
-        queue_inst.pop();
+        queue_inst->pop();
     }
     std::cout << "Finished execution" << std::endl;
     return 0;
