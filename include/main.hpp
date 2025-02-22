5b789aca5a4a239df5cb416f22f487ea3c7cffa0
Max B. <77401015+maxboro@users.noreply.github.com>
2025-02-22 10:42:43 +0200

    Add settings in file (#10)
    * Add settings
    
    * Add settings block to README

--- include/main.hpp
+++ include/main.hpp
@@ -10,6 +10,7 @@
 #include "producer.hpp"
 #include "consumer.hpp"
 #include "good.h"
+#include "settings.hpp"
 
 // Global atomic flag to signal threads to stop
 std::atomic<bool> stop_flag(false);
@@ -19,26 +20,32 @@ void handle_sigint(int signal) {
     stop_flag.store(true);
 }
 
-// Simple queue use example
 int main(){
+    std::cout << "Running" << std::endl;
+
     // Register the signal handler
     std::signal(SIGINT, handle_sigint);
 
-    std::cout << "Running" << std::endl;
+    // Load params from settings
+    Settings* settings = Settings::get_instance();
+    settings->load_settings();
+    int n_producers = settings->get_value("n_producers");
+    int n_consumers = settings->get_value("n_consumers");
+
     auto queue_ptr = std::make_shared<std::queue<Good>>();
     auto mutex_ptr = std::make_shared<std::mutex>();
     
     std::vector<std::thread> threads;
 
     // producer threads
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < n_producers; i++){
         threads.emplace_back(produce, queue_ptr, mutex_ptr, &stop_flag, i);
     }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     // consumer threads
-    for (int i = 0; i < 2; i++){
+    for (int i = 0; i < n_consumers; i++){
         threads.emplace_back(consume, queue_ptr, mutex_ptr, &stop_flag, i);
     }
 
